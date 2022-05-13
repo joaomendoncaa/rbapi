81ee10dc9ec5506ff7a73d5d5b3bdd4588685223
joaomendoncaa <joaommm0312@gmail.com>
2022-05-13 18:03:15 +0100

    connection rusqlite rocket..

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use rusqlite::Connection;
+
 #[macro_use]
 extern crate rocket;
 
@@ -8,8 +10,28 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    println!("Application ignited!");
+#[get("/user/<name>")]
+fn endpoint_handler_user(name: String) -> String {
+    let response: String = format!("Hello, {}!", &name);
+    response
+}
 
-    rocket::ignite().mount("/", routes![index]).launch();
+fn db_connect() {
+    let db_connection: Connection =
+        rusqlite::Connection::open("data.sqlite").expect("Error connecting to the database!");
+
+    db_connection
+        .execute(
+            "create table if not exists main (id integer primary key);",
+            [],
+        )
+        .expect("Couldn't create the table");
+}
+
+fn main() {
+    db_connect();
+
+    rocket::ignite()
+        .mount("/", routes![index, endpoint_handler_user])
+        .launch();
 }

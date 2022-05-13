2b5c7d0f7ef59856c53862fb823851030b29bfa5
joaomendoncaa <joaommm0312@gmail.com>
2022-05-13 14:35:40 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    println!("Application ignited!");
+
+    rocket::ignite().mount("/", routes![index]).launch();
+}
